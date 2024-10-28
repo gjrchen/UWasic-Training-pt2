615f632220d8199ad8527baa24929b8ec650e873
Siddharth Nema <siddharthnema04@gmail.com>
2024-10-28 15:26:17 -0400

    Create cocotb testbench

--- test/tb.v
+++ test/tb.v
@@ -13,33 +13,18 @@ module tb ();
     #1;
   end
 
-  // Wire up the inputs and outputs:
+  // Init registers 
+  reg resetn;
   reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+  
+  reg [3:0] opcode;
+  wire [14:0] out_res;
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
-
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(1'b1),
-      .VGND(1'b0),
-`endif
-
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
+  tt_um_control_block uut(
+    .clk(clk), 
+    .resetn(resetn),
+    .opcode(opcode),
+    .out(out_res)
   );
 
 endmodule
--- test/test.py
+++ test/test.py
@@ -11,30 +11,43 @@ async def test_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    clock = Clock(dut.clk, 10, units="ns")
     cocotb.start_soon(clock.start())
 
     # Reset
+    dut.resetn.vaue = 0
     dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 30)
+    dut.resetn.value = 1
 
-    dut._log.info("Test project behavior")
+    dut._log.info("Test NOP")
+    dut.opcode.value = 1
+    await ClockCycles(dut.clk, 1000)
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    dut._log.info("Test ADD")
+    dut.opcode.value = 2
+    await ClockCycles(dut.clk, 1000)
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    dut._log.info("Test SUB")
+    dut.opcode.value = 3
+    await ClockCycles(dut.clk, 1000)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    dut._log.info("Test LDA")
+    dut.opcode.value = 4
+    await ClockCycles(dut.clk, 1000)
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    dut._log.info("Test OUT")
+    dut.opcode.value = 5
+    await ClockCycles(dut.clk, 1000)
+
+    dut._log.info("Test STA")
+    dut.opcode.value = 6
+    await ClockCycles(dut.clk, 1000)
+
+    dut._log.info("Test JMP")
+    dut.opcode.value = 7
+    await ClockCycles(dut.clk, 1000)
+
+    dut._log.info("Test HLT")
+    dut.opcode.value = 0
+    await ClockCycles(dut.clk, 1000) 
