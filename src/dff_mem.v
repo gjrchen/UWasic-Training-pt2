379b13c3355d34008c92569fb960bcedf2a17d95
Eshaan Mehta <eshaanm.mehta@gmail.com>
2024-11-04 16:23:38 -0500

    Update dff_mem.v again

--- src/dff_mem.v
+++ src/dff_mem.v
@@ -6,10 +6,8 @@ module tt_um_dff_mem #(
     input  wire [3:0] addr,
     input  wire [7:0] data_in,
     output wire [7:0] data_out,
-    output wire [7:0] data_enable,
     input  wire       lr_n,     //load/write enable
     input  wire       ce_n,     // read enable
-    // input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
@@ -18,11 +16,9 @@ module tt_um_dff_mem #(
 
   // Assign outputs based on ce_n and lr_n control signals
   assign data_out = (!ce_n) ? RAM[addr] : 8'bZ;  // Output data when ce_n is low
-  assign data_enable = (!ce_n) ? 8'hFF : 8'h00;     // Enable output when ce_n is low
 
   always @(posedge clk) begin
     if (!rst_n) begin
-      // uo_out <= 8'b0;
       for (int i = 0; i < RAM_BYTES; i++) begin
         RAM[i] <= 8'b0;  // Reset RAM contents
       end
@@ -30,7 +26,6 @@ module tt_um_dff_mem #(
       if (!lr_n) begin  // Load data into RAM when lr_n is low
           RAM[addr] <= data_in;
       end
-      // uo_out <= 8'b0;  // Optional: can be used for debugging or status
     end
   end
 
