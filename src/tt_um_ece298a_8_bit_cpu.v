9ad88b097395b5de90bcb545e6da28f64cdeca42
Siddharth Nema <siddharthnema04@gmail.com>
2024-11-04 19:57:28 -0500

    cb test 3

--- src/tt_um_ece298a_8_bit_cpu.v
+++ src/tt_um_ece298a_8_bit_cpu.v
@@ -16,55 +16,55 @@ module tt_um_ece298a_8_bit_cpu_top (
     input  wire rst_n           // reset_n - low to reset
 );
 
-    // // Bus //
-    // wire [7:0] bus;                 // Bus (8-bit) (High impedance when not in use)
-    // wire [3:0] bus4bit;             // 4-bit Bus (lower 4 bits of the 8-bit Bus) (High impedance when not in use)
-    // assign bus4bit = bus[3:0];      // Assign 4-bit Bus to the lower 4 bits of the 8-bit Bus 
+    // Bus //
+    wire [7:0] bus;                 // Bus (8-bit) (High impedance when not in use)
+    wire [3:0] bus4bit;             // 4-bit Bus (lower 4 bits of the 8-bit Bus) (High impedance when not in use)
+    assign bus4bit = bus[3:0];      // Assign 4-bit Bus to the lower 4 bits of the 8-bit Bus 
 
     // // Control Signals //
-    // wire [14:0] control_signals;
+    wire [14:0] control_signals;
 
     // // Wires //
-    // wire [3:0] opcode;              // opcode from IR to Control
-    // wire [7:0] reg_a;               // value from Accumulator Register to ALU
-    // wire [7:0] reg_b;               // value from B Register to ALU
+    wire [3:0] opcode;              // opcode from IR to Control
+    wire [7:0] reg_a;               // value from Accumulator Register to ALU
+    wire [7:0] reg_b;               // value from B Register to ALU
     
     // // ALU Flags //
-    // wire CF;                        // Carry Flag
-    // wire ZF;                        // Zero Flag
+    wire CF;                        // Carry Flag
+    wire ZF;                        // Zero Flag
 
-    // // Wire between MAR and RAM //
-    // wire [7:0] mar_to_ram_data;
-    // wire [3:0] mar_to_ram_addr;
+    // Wire between MAR and RAM //
+    wire [7:0] mar_to_ram_data;
+    wire [3:0] mar_to_ram_addr;
 
-    // // Control Signals for the Program Counter //
-    // wire Cp = control_signals[14];     // 
-    // wire Ep = control_signals[13];     // 
-    // wire Lp = control_signals[12];     // 
+    // Control Signals for the Program Counter //
+    wire Cp = control_signals[14];     // 
+    wire Ep = control_signals[13];     // 
+    wire Lp = control_signals[12];     // 
 
-    // // Control Signals for the RAM //
-    // wire nLma = control_signals[11];   // 
-    // wire nLmd = control_signals[10];   // 
-    // wire nCE = control_signals[9];     // 
-    // wire nLr = control_signals[8];     // 
+    // Control Signals for the RAM //
+    wire nLma = control_signals[11];   // 
+    wire nLmd = control_signals[10];   // 
+    wire nCE = control_signals[9];     // 
+    wire nLr = control_signals[8];     // 
 
-    // // Control Signals for the Instruction Register //
-    // wire nLi = control_signals[7];     // enable Instruction Register load from bus (ACTIVE-LOW)
-    // wire nEi = control_signals[6];      // enable Instruction Register output to the bus (ACTIVE-LOW)
+    // Control Signals for the Instruction Register //
+    wire nLi = control_signals[7];     // enable Instruction Register load from bus (ACTIVE-LOW)
+    wire nEi = control_signals[6];      // enable Instruction Register output to the bus (ACTIVE-LOW)
 
-    // // Control Signals for the Accumulator Register //
-    // wire nLa = control_signals[5];     // enable Accumulator Register load from bus (ACTIVE-LOW)
-    // wire Ea = control_signals[4];      // enable Accumulator Register output to the bus (ACTIVE-HIGH)
+    // Control Signals for the Accumulator Register //
+    wire nLa = control_signals[5];     // enable Accumulator Register load from bus (ACTIVE-LOW)
+    wire Ea = control_signals[4];      // enable Accumulator Register output to the bus (ACTIVE-HIGH)
 
-    // // Control Signals for the ALU //
-    // wire sub = control_signals[3];     // perform addition when 0, perform subtraction when 1
-    // wire Eu = control_signals[2];      // enable ALU output to the bus (ACTIVE-HIGH)
+    // Control Signals for the ALU //
+    wire sub = control_signals[3];     // perform addition when 0, perform subtraction when 1
+    wire Eu = control_signals[2];      // enable ALU output to the bus (ACTIVE-HIGH)
 
-    // // Control Signals for the B Register //
-    // wire nLb = control_signals[1];     // enable B Register load from bus (ACTIVE-LOW)
+    // Control Signals for the B Register //
+    wire nLb = control_signals[1];     // enable B Register load from bus (ACTIVE-LOW)
 
-    // // Control Signals for the Output Register //
-    // wire nLo = control_signals[0];     // 
+    // Control Signals for the Output Register //
+    wire nLo = control_signals[0];     // 
     
     
     // // Program Counter //
