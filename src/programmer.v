71256034a920a71021c19d8f0a4a269bd22b2019
Roni Kant <35043400+Penguronik@users.noreply.github.com>
2024-11-06 00:45:45 -0500

    Update programmer.v

--- src/programmer.v
+++ src/programmer.v
@@ -48,7 +48,9 @@ parameter T0 = 0, T1 = 1, T2 = 2, T3 = 3, T4 = 4, T5 = 5;
   always @(posedge clk) begin
     if (!resetn || !programming_stage) begin // Check if reset is asserted or not in programming_stage, if yes, put into a holding stage
       stage <= 6;
-      ram_addr <= 0;
+    end
+    if (!resetn) begin
+       ram_addr <= 0;
     end
  	else begin                   // If reset is not asserted, do the stages sequentially
       if (stage == 6) begin        
