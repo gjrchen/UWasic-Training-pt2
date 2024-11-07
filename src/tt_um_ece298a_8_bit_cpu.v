085607340c1b2928e4e4a4ffd0ab99afb655a4bc
Damir Gazizullin <dgazizul@uwaterloo.ca>
2024-11-07 12:53:24 -0500

    Remove unused signals from warning suppression in CPU module

--- src/tt_um_ece298a_8_bit_cpu.v
+++ src/tt_um_ece298a_8_bit_cpu.v
@@ -189,6 +189,6 @@ module tt_um_ece298a_8_bit_cpu_top (
     assign uio_out[0] = 1'b0;         // Set the IO outputs to 0
     assign uio_oe = 8'b00111110;          // Configure the IO ports to be inputs
 
-    wire _unused = &{uio_in[7:3], ena, ZF, CF}; // Avoid unused variable warning
+    wire _unused = &{uio_in[7:6], ena}; // Avoid unused variable warning
 
 endmodule
