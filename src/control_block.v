9ddeedbc537e0cab7fd288aaf2a723d5e06c4de0
Damir Gazizullin <dgazizul@uwaterloo.ca>
2024-11-09 02:25:14 -0500

    Update control block to handle reset condition on negedge clk

--- src/control_block.v
+++ src/control_block.v
@@ -83,7 +83,7 @@ always @(posedge clk) begin
 end
 
 /* Micro-Operation Logic */
-always @(negedge clk) begin
+always @(negedge clk or negedge resetn) begin
     control_signals <= 15'b000111111100011; // All signals are deasserted
     done_load_reg <= 0;
     read_ui_in_reg <= 0;
