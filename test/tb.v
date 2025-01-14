721b820527de753b1a2d6752d39124bb9c562181
Gerry Chen <83669998+gjrchen@users.noreply.github.com>
2025-01-13 22:47:19 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -7,7 +7,7 @@
 module tb ();
   // Dump the signals to a VCD file. You can view it with gtkwave.
   initial begin
-    $dumpfile("tb.vcd");
+     $dumpfile("oogabooga.vcd");
     $dumpvars(0, tb);
     #1;
   end
