b1538f2d2c70163bbdad8a6966f48e87c28a6025
Gerry Chen <83669998+gjrchen@users.noreply.github.com>
2025-01-13 22:48:15 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -27,7 +27,7 @@ reg [7:0] uio_oe;
 `endif
 
 // Replace tt_um_example with your module name:
-tt_um_ece298a_8_bit_cpu_top user_project (
+tt_um_example user_project (
 // Include power ports for the Gate Level test:
 `ifdef GL_TEST
       .VPWR(VPWR),
